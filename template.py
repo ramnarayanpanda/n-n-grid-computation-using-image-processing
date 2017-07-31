ccfeee923b4dc1ce721ae39cc24def18365e5e44
ramnarayanpanda <ramkanhaaa@gmail.com>
2017-07-31 15:46:18 +0530

    Update template.py

--- template.py
+++ template.py
@@ -11,6 +11,7 @@ for i in range(0,12):
 
 lst=[]
 
+#here in place of 6, you can take the value of grid by seeing your grid
 for i in range(6):
     for j in range(6):
         x=''
@@ -27,6 +28,8 @@ for i in range(6):
             d=[]
                 
         else:
+            #100 because my image have grids with size 100*100
+            #-5 helps to get rid of the black pixels present while croping the image, so is +5
             crop_img=img[100*i+5:100*(i+1)-5,100*j+5:100*(j+1)-5]
             for k in range(12):
                 res = cv2.matchTemplate(digit[k],crop_img,cv2.TM_CCOEFF_NORMED)
@@ -39,6 +42,8 @@ print(m)
 font=cv2.FONT_HERSHEY_SIMPLEX
 k=0
 for i in m:
+    #if else condition is used to adjust the result with in the grid
+    #520 and 70, i have taken by multiple trials
     if len(i)==1:
         cv2.putText(img, i, (520,70+k), font, 2.15, (0,0,0), 2, cv2.LINE_AA)
     else:
